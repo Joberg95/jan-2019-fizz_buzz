934d52232798725393ee5e5012ece773244eacb8
nevroje <joakim.berglind@gmail.com>
2019-01-10 19:13:41 +0100

    Updated code to function as in readme file

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,9 +1,9 @@
 def fizz_buzz(number)
     if number % 15 == 0
-      'fizzbuzz'
-    elseif number % 5 == 0
-      'buzz'
-    elseif number % 3 == 0
-      'fizz'
+      'Fizz buzz'
+    elsif number % 5 == 0
+      'Buzz'
+    elsif number % 3 == 0
+      'Fizz'
     end 
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,13 +1,15 @@
 require './lib/fizz_buzz'
 
 describe 'fizz_buzz' do
-    it "returns 'fizz_buzz' if number is divisible by 15" do
-    expect(fizz_buzz(15)).to eq 'fizz_buzz'
+    it 'returns Fizz buzz if number is divisible by 15' do
+    expect(fizz_buzz(15)).to eq 'Fizz buzz'
     end
-    it "returns 'buzz' if number is divisible by 5" do
-    expect(buzz(5)).to eq 'buzz'
+
+    it 'returns Buzz if number is divisible by 5' do
+    expect(fizz_buzz(5)).to eq 'Buzz'
     end
-    it "returns 'fizz' if number is divisble by 3"  do
-    expect(fizz(3)).to eq 'fizz'
+
+    it 'returns fizz_buzz if number is divisble by 3'  do
+    expect(fizz_buzz(3)).to eq 'Fizz'
     end
 end
